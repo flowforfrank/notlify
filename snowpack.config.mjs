1a5c33066b6f57eb721e2e49f2fa8b75e5af3a4b
flowforfrank <hello@allma.si>
2021-06-30 13:02:58 +0200

    💄 Add Sass support

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,28 +1,27 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    /* ... */
-    public: '/',
-    src: '/'
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    mount: {
+        public: '/',
+        src: '/'
+    },
+    plugins: [
+        '@snowpack/plugin-sass'
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+        /* ... */
+    },
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './scss/index.scss';
+
 ReactDOM.render(<div>👋</div>, document.getElementById('root'));
