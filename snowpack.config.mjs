bca5ae1106fb5a2904bcbc6f759f106f655ba457
flowforfrank <hello@allma.si>
2021-06-30 12:44:29 +0200

    🎨 Reorganize folders

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -2,6 +2,8 @@
 export default {
   mount: {
     /* ... */
+    public: '/',
+    src: '/'
   },
   plugins: [
     /* ... */
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>👋</div>, document.getElementById('root'));
